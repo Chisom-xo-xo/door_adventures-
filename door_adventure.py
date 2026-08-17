d046c1c87a00e38067d9801603eca141511e4b04
Chisom Agbarakwe <tj7b5g2z8z@privaterelay.appleid.com>
2026-08-17 08:26:01 +0100

    Add door adventure game working with Location A logic

---
+++ door_adventure.py
@@ -0,0 +1,55 @@
+print('=' * 50)
+print('WELCOME TO THE DOOR ADVENTURE')
+print('=' * 50)
+#The player has to complete the first adventure to unlock the second adventure.
+print('Before you lies two adventures: Adventure A and Adventure B')
+print('=' * 50)
+print('WELCOME TO ADVENTURE A')
+print('=' * 50)
+#When the player enters the first door, they have three accessible doors to pick from.
+print('\nBefore you lies three doors: Door A, Door B, Door C.')
+#For now, A is the only available path
+while True:
+    choice=input('Which door do you choose? (Door A, B, or C)\n>')
+    choice=choice.upper()
+    if choice=='A' or choice=='B' or choice=='C':
+        break
+    else:
+        print('\nDoor does not exist. Please choose A, B, or C.\n')
+print('\nYou chose Door' + choice)
+#LOCATION A
+if choice=='A':
+    while True:
+        print('=' * 50)
+        print('WELCOME TO LOC A')
+        print('=' * 50)
+        print('Solve this clue to enter the world')
+        print('CLUE:PLACEHOLDER CLUE LOCA')
+        while True:
+            answer=input('>')
+#answer: placeholder answer loca
+            answer=answer.lower()
+            if answer=='placeholder answer loca':
+                print('\nCorrect! You may continue\n')
+                break
+            else:
+                print('\nIncorrect! Try again.\n')
+        tries=0
+        while tries<3:
+            print('CLUE: PLACEHOLDER CLUE LOCAi')
+            answer_Ai=input('>')
+#answer: placeholder answer locai
+            answer_Ai=answer_Ai.lower()
+            if answer_Ai=='placeholder answer locai':
+                print('\nCorrect! You may continue.\n')
+                break
+            else:
+                tries=tries+1
+                print('\nIncorrect. Tries left: ' + str(3-tries))
+        if tries==3:
+            print('\nYou failed too many times. Returning to LocA.\n')
+            continue
+        else:
+            break
+            
+        
